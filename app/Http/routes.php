4f0a31722081dd64a3433e684bd093cdfdd77268
Nastya Ivanova <nastasya_ivanova_2015@mail.ru>
2018-08-17 10:44:18 +0400

    new version

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,16 +22,11 @@
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-
-    Route::get('/', function () {
-        return view('welcome');
-    })->middleware('guest');
-
-    Route::get('/tasks', 'TaskController@index');
-    Route::post('/task', 'TaskController@store');
-    Route::delete('/task/{task}', 'TaskController@destroy');
 
     Route::auth();
-
-});
+    Route::resource('tasks','TaskController');
+    Route::post('/task', 'TaskController@store');
+    Route::get('/tasks', 'TaskController@index');
+    Route::delete('/task/{task}', 'TaskController@destroy');
+    Route::get('/', function () {
+    return view('welcome');});
