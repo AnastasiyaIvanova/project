b47440992f9c7a21b7ebb19ae9b6bc3d302f79f7
Nastya Ivanova <nastasya_ivanova_2015@mail.ru>
2018-08-18 23:03:15 +0400

    work please

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Task;
+use App\Policies\TaskPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Task' => 'App\Policies\TaskPolicy',
+        Task::class => TaskPolicy::class,
+        // 'App\Task' => 'App\Policies\TaskPolicy',
     ];
 
     /**
